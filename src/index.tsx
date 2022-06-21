89fdeb1ebeba5e0e3794c28e54a598bc8f91a69d
Natálie Sedláková <natalie.sedlakova@betrian.cz>
2022-06-21 18:52:26 +0200

    responzivita pozadí

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import Table from "./organisms/Table";
 
 const App = () => {
   return (
-    <div className="flex min-h-screen w-screen justify-center items-center flex-col bg-cover bg-[url('/assets/swamp.jpeg')] p-10"
+    <div className="flex min-h-screen w-screen justify-center items-center flex-col bg-cover bg-[url('/assets/swamp.jpeg')] bg-fixed p-10"
     >
       <h1 className="mb-16 text-4xl md:text-7xl font-bold font-serif text-white text-shadow-h1">
         Lesný duch
